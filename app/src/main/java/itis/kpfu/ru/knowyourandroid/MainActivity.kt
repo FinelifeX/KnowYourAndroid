36390907fad61ad7e7d09cab1d326d6a78c8095a
FinelifeX <random.finelife@gmail.com>
2018-07-06 16:16:04 +0300

    Initial commit

---
+++ app/src/main/java/itis/kpfu/ru/knowyourandroid/MainActivity.kt
@@ -0,0 +1,12 @@
+package itis.kpfu.ru.knowyourandroid
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
