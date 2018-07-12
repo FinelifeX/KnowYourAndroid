84ae36c12817df810dcae8b2dce583a4f77b17ea
IlyaZakharchenko <zakharchenkoilyajob@gmail.com>
2018-07-12 19:48:01 +0300

    Made sign in & sign up with email + password & with Google account

---
+++ app/src/main/java/itis/kpfu/ru/knowyourandroid/ResetPasswordActivity.kt
@@ -0,0 +1,75 @@
+package itis.kpfu.ru.knowyourandroid
+
+import android.os.Bundle
+import android.support.design.widget.BaseTransientBottomBar.BaseCallback
+import android.support.design.widget.Snackbar
+import android.support.design.widget.Snackbar.LENGTH_LONG
+import android.support.v7.app.AppCompatActivity
+import android.text.Editable
+import android.text.TextUtils
+import android.text.TextWatcher
+import android.view.View
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_reset_password.btn_reset
+import kotlinx.android.synthetic.main.activity_reset_password.container
+import kotlinx.android.synthetic.main.activity_reset_password.et_email
+import kotlinx.android.synthetic.main.activity_reset_password.it_email
+import kotlinx.android.synthetic.main.activity_reset_password.progress_bar
+
+class ResetPasswordActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reset_password)
+        initClickListeners()
+        initTextListeners()
+
+        val email = intent.getStringExtra(EMAIL)
+        if (email != null) {
+            et_email.setText(email)
+        }
+    }
+
+    private fun initClickListeners() {
+        btn_reset.setOnClickListener {
+            val email = et_email.text.toString()
+
+            if (TextUtils.isEmpty(email)) {
+                it_email.error = getString(R.string.error_empty_email)
+                return@setOnClickListener
+            }
+            if (!email.matches(EMAIL_REGEX.toRegex())) {
+                it_email.error = getString(R.string.error_wrong_email)
+                return@setOnClickListener
+            }
+            progress_bar.visibility = View.VISIBLE
+            FirebaseAuth.getInstance().sendPasswordResetEmail(email).addOnCompleteListener {
+                if (it.isSuccessful) {
+                    progress_bar.visibility = View.GONE
+                    Snackbar.make(container, R.string.reset_success,
+                            LENGTH_LONG).addCallback(object : BaseCallback<Snackbar>() {
+                        override fun onDismissed(transientBottomBar: Snackbar?, event: Int) {
+                            finish()
+                        }
+                    }).show()
+                } else {
+                    progress_bar.visibility = View.GONE
+                    Snackbar.make(this.container, it.exception?.message.toString(), LENGTH_LONG)
+                }
+            }
+        }
+    }
+
+    private fun initTextListeners() {
+        et_email.addTextChangedListener(object : TextWatcher {
+
+            override fun afterTextChanged(s: Editable) {}
+
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                it_email.error = null
+            }
+        })
+    }
+}
---
+++ app/src/main/java/itis/kpfu/ru/knowyourandroid/UserProvider.kt
@@ -0,0 +1,58 @@
+package itis.kpfu.ru.knowyourandroid
+
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+
+/**
+ * Created by Ilya Zakharchenko on 12.07.2018.
+ */
+object UserProvider {
+
+    private val dbRef = FirebaseDatabase.getInstance().reference
+
+    private val uid = FirebaseAuth.getInstance().uid.toString()
+
+    private var provider: UserProvider? = null
+
+    var listener: UserProviderOnCompleteListener? = null
+
+    fun addOnCompleteListener(listenerImpl: UserProviderOnCompleteListener) {
+        listener = listenerImpl
+    }
+    private var user: User? = null
+
+    fun getInstance(): UserProvider? {
+        if (provider == null) {
+            provider = UserProvider
+        }
+        return provider
+    }
+
+    fun provideUser(): UserProvider? {
+        if (user == null) {
+            dbRef.child("users").child(uid).addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+                    user = dataSnapshot.getValue(User::class.java)
+                    listener?.onComplete()
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    // Failed to read value
+                }
+            })
+        }
+        return this.provider
+    }
+
+    fun createUser(user: User) {
+        dbRef.child("users").child(uid).setValue(user)
+        this.user = user
+    }
+
+    fun clear() {
+        user = null
+    }
+}
